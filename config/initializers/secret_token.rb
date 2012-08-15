02c48c8148492e35c6b9cf2ceb40b30778bfe9a4
Nieve <nievegoor@gmail.com>
2012-08-15 21:43:26 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nivter::Application.config.secret_token = '44332c469ebd788d1503b879878de0dbd528e7f2a5b85c9c6ebc4abc8a9d42fdddcfcf45d38cd3b0a479c61529821c25c94b8ee1b9b2fb8991ee06c367ca9ae1'
