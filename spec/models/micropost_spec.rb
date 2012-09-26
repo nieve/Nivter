97578d9a6a75885aaa75766ba9d1487b1304d366
Nieve <nievegoor@gmail.com>
2012-09-26 22:28:27 +0100

    display posts

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -13,6 +13,16 @@ describe Micropost do
   	before {@micropost.user_id = nil}
   	it {should_not be_valid}
   end
+  describe "content property" do
+    describe "when nil" do
+      before {@micropost.content = nil}
+      it {should_not be_valid}
+    end
+    describe "when beyond 140 characters" do
+      before {@micropost.content = 'a' * 141}
+      it {should_not be_valid}
+    end
+  end
   it "should not allow access to user id" do
   	expect {Micropost.new(user_id: 42)}.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
   end
